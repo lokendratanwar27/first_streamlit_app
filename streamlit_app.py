654d843ccc457eda9d597fd49db27e1b293da176
lokendratanwar27 <66329843+lokendratanwar27@users.noreply.github.com>
2023-05-05 15:51:56 +0530

    Update streamlit_app.py
    added emojis and fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('BREAKFAST MENU')
-streamlit.text('blueberry 3 & oatmeal')
-streamlit.text('kale,spinach and ricket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 blueberry 3 & oatmeal')
+streamlit.text('🥗 kale,spinach and ricket smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🍞🥑 Avocado Toast')
 
 
