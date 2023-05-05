5330964db97a7d9bfc2914e55c9073c1dbe4b446
lokendratanwar27 <66329843+lokendratanwar27@users.noreply.github.com>
2023-05-05 15:56:03 +0530

    Update streamlit_app.py
    added csv files from s3 bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('BREAKFAST MENU')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 blueberry 3 & oatmeal')
 streamlit.text('🥗 kale,spinach and ricket smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🍞🥑 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
