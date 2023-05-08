b9158db436b8f694cfda1bbfe1b0be10fb0c632f
lokendratanwar27 <66329843+lokendratanwar27@users.noreply.github.com>
2023-05-08 16:07:01 +0530

    Update streamlit_app.py
    added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 import pandas
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('BREAKFAST MENU')
